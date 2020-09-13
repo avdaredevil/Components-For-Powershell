9d82baffbf16fec7df4ed2dae56caa5a8eb5e94b
Apoorv Verma [AP] <avdaredevil@gmail.com>
2020-09-12 22:44:50 -0400

    Hotfix to allow imports for git commands even if GIT isn't where it is expected

--- Configure-Component.ps1
+++ Configure-Component.ps1
@@ -142,12 +142,15 @@ $Components = @{
         if (!$Silent) {Write-AP-Wrapper "+Configured Dr. Racket for AP-PShell Management Console!"}
         rv PYD}
     Git = {
-        $PyD = item ((,"C:\Git\bin*")+
-            (Get-PSDrive | ? {$_.Provider.Name -eq "FileSystem"} | % Root | % {
-                Join-Path "$_" "AP-Langs\Git\bin"
-            })) -ea SilentlyContinue | % FullName | Select -f 1
-        if (!(test-path $Pyd)) {Throw "Git Does not Exist on System!";exit}
-        A2Path $PyD
+        if (!(AP-Require "dep:git" -PassThru)) {
+            $PyD = item ((,"C:\Git\bin*")+
+                (Get-PSDrive | ? {$_.Provider.Name -eq "FileSystem"} | % Root | % {
+                    Join-Path "$_" "AP-Langs\Git\bin"
+                })) -ea SilentlyContinue | % FullName | Select -f 1
+            if (!(test-path $Pyd)) {Throw "Git Does not Exist on System!";exit}
+            A2Path $PyD
+            rv PYD
+        }
         function Global:Git-RebaseDll ([Switch]$Silent) {
             pushd (cmd /c where msys-1.0.dll)
             $addr = "0x$(3..7 | Get-Random)0000000"
@@ -207,7 +210,7 @@ $Components = @{
             git tag -a $Version -m $Message;git push --tags
         }
         if (!$Silent) {Write-AP-Wrapper "+Configured Git Console for AP-PShell Management Console!"}
-        rv PYD}
+    }
     Go = {
         $PyDs = @(item ((,"C:\Go*")+
             (Get-PSDrive | ? {$_.Provider.Name -eq "FileSystem"} | % Root | % {
